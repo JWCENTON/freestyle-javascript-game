2800ea75d34f467ea31048f2439381b8a4839f51
Jacek Walaszek <w.jacek99@gmail.com>
2022-08-16 22:17:15 +0200

    refactoring

--- game.js
+++ game.js
@@ -1,21 +1,22 @@
-let apple = { x: 10, y: 10 };
+let applePosition = { x: 10, y: 10 };
 
 function drawApple() {
     const appleElement = document.createElement('div')
-    appleElement.style.gridRowStart = apple.y + 'px'
-    appleElement.style.gridColumnStart = apple.x + 'px'
+    appleElement.style.gridRowStart = applePosition.y + 'px'
+    appleElement.style.gridColumnStart = applePosition.x + 'px'
     appleElement.classList.add('apple', 'item')
     let gameBoard = document.querySelector('.game-container');
     gameBoard.appendChild(appleElement)
 };
 
-drawApple();
 
 initGame();
 
 function initGame() {
 
     // Your game can start here, but define separate functions, don't write everything in here :)
+
+    drawApple();
 
 }
 
