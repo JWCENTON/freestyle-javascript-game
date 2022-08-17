21501126efc5a8353087214101260b761a6f9576
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2022-08-17 12:56:44 +0200

    snake move fixed

--- game.js
+++ game.js
@@ -11,52 +11,58 @@ function drawApple() {
 
 
 /** direction: R, L, U, D */
-let direction = 'R';
-let snakePosition = [{x: 4, y: 1},
+let direction = 'D';
+let snakePosition = [
+    {x: 4, y: 1},
     {x: 3, y: 1},
-    {x: 2, y: 1}]
+    {x: 2, y: 1}
+];
 
+
+function updatePosition() {
+    const last_x = snakePosition[snakePosition.length - 1].x;
+    const last_y = snakePosition[snakePosition.length - 1].y;
+    for (let index = snakePosition.length - 1; index > 0; index--) {
+        snakePosition[index].x = snakePosition[index - 1].x;
+        snakePosition[index].y = snakePosition[index - 1].y;
+    }
+    switch (direction) {
+        case "R":
+            snakePosition[0].x++;
+            break;
+        case "L":
+            snakePosition[0].x--;
+            break;
+        case "U":
+            snakePosition[0].y--;
+            break;
+        case "D":
+            snakePosition[0].y++;
+            break;
+    }
+    /* if snakePosition[0] enters apple then add new snake segment (last_x, last_y) */
+}
 
 function moveSnake() {
-    let snakeElement = document.getElementById('snake');
+    let snakeElement = document.getElementById('snake0');
     if (snakeElement === null) {
-        for (let i = 0; i < snakePosition.length; i++) {
-        snakeElement = document.createElement('div');
-        snakeElement.style.gridRowStart = snakePosition[i].y;
-        snakeElement.style.gridColumnStart = snakePosition[i].x;
-        snakeElement.classList.add('snake-element');
-        snakeElement.id = "snake";
         let gameBoard = document.querySelector('.game-container');
-        gameBoard.appendChild(snakeElement);
+        for (let i = 0; i < snakePosition.length; i++) {
+            snakeElement = document.createElement('div');
+            snakeElement.style.gridRowStart = snakePosition[i].y;
+            snakeElement.style.gridColumnStart = snakePosition[i].x;
+            snakeElement.classList.add('snake-element');
+            snakeElement.id = "snake" + i.toString();
+            gameBoard.appendChild(snakeElement);
         }
     } else {
-        switch (direction) {
-            case "R":
-                snakePosition[0].x++;
-                snakePosition[1] = snakePosition[0];
-                snakePosition[2] = snakePosition[1];
-                // snakePosition[1].x++;
-                // snakePosition[2].x++;
-                break;
-            case "L":
-                snakePosition.x--;
-                break;
-            case "U":
-                snakePosition.y--;
-                break;
-            case "D":
-                snakePosition.x++;
-                break;
+        updatePosition();
+        for (let i = 0; i < snakePosition.length; i++) {
+            snakeElement = document.getElementById("snake" + i.toString())
+            snakeElement.style.gridRowStart = snakePosition[i].y;
+            snakeElement.style.gridColumnStart = snakePosition[i].x;
         }
-        snakeElement.style.gridRowStart = snakePosition[0].y;
-        snakeElement.style.gridColumnStart = snakePosition[0].x;
-        console.log('Ruszam się 1');
-        snakeElement.style.gridRowStart = snakePosition[1].y;
-        snakeElement.style.gridColumnStart = snakePosition[1].x;
-        console.log('Ruszam się 2');
-        snakeElement.style.gridRowStart = snakePosition[2].y;
-        snakeElement.style.gridColumnStart = snakePosition[2].x;
-        console.log('Ruszam się 3');
+
     }
 }
 
