913ca03ed50b70967d1c82c15565368baf984205
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2022-08-17 22:36:47 +0200

    fix merge

--- game.js
+++ game.js
@@ -27,44 +27,41 @@ function getRandomApplePosition() {
 }
 
 /** direction: R, L, U, D */
-let direction = 'R';
-
+let direction = "D";
 let snakePosition = [
-    {x: 4, y: 1},
-    {x: 3, y: 1},
-    {x: 2, y: 1}
+  { x: 4, y: 1 },
+  { x: 3, y: 1 },
+  { x: 2, y: 1 },
 ];
 
 function getDirection() {
-    return direction
+  return direction;
 }
 
 function updatePosition() {
-    const last_x = snakePosition[snakePosition.length - 1].x;
-    const last_y = snakePosition[snakePosition.length - 1].y;
-    // if (snakePosition[0].x <= 20 || snakePosition[0].x >= 1 || snakePosition[0].y >= 1 || snakePosition[0].x <= 20)
-        const direction = getDirection();
-        console.log("Direction: " + direction);
-        for (let index = snakePosition.length - 1; index > 0; index--) {
-            snakePosition[index].x = snakePosition[index - 1].x;
-            snakePosition[index].y = snakePosition[index - 1].y;
-        }
-        switch (direction) {
-            case "R":
-                snakePosition[0].x++;
-                break;
-            case "L":
-                snakePosition[0].x--;
-                break;
-            case "U":
-                snakePosition[0].y--;
-                break;
-            case "D":
-                snakePosition[0].y++;
-                break;
-        }
-        /* if snakePosition[0] enters apple then add new snake segment (last_x, last_y) */
-
+  const last_x = snakePosition[snakePosition.length - 1].x;
+  const last_y = snakePosition[snakePosition.length - 1].y;
+  const direction = getDirection();
+  console.log("Direction: " + direction);
+  for (let index = snakePosition.length - 1; index > 0; index--) {
+    snakePosition[index].x = snakePosition[index - 1].x;
+    snakePosition[index].y = snakePosition[index - 1].y;
+  }
+  switch (direction) {
+    case "R":
+      snakePosition[0].x++;
+      break;
+    case "L":
+      snakePosition[0].x--;
+      break;
+    case "U":
+      snakePosition[0].y--;
+      break;
+    case "D":
+      snakePosition[0].y++;
+      break;
+  }
+  /* if snakePosition[0] enters apple then add new snake segment (last_x, last_y) */
 }
 
 function moveSnake() {
@@ -102,103 +99,61 @@ function moveSnake() {
       snakeElement.id = "snake" + i.toString();
       gameBoard.appendChild(snakeElement);
       console.log(`${snakeElement.id} - ${snakeElement.classList}`);
-    // if (snakePosition[0].x <= 20 || snakePosition[0].x >= 1 || snakePosition[0].y >= 1 || snakePosition[0].x <= 20)
-        const direction = getDirection();
-        console.log("Direction: " + direction);
-        for (let index = snakePosition.length - 1; index > 0; index--) {
-            snakePosition[index].x = snakePosition[index - 1].x;
-            snakePosition[index].y = snakePosition[index - 1].y;
-        }
-        switch (direction) {
-            case "R":
-                snakePosition[0].x++;
-                break;
-            case "L":
-                snakePosition[0].x--;
-                break;
-            case "U":
-                snakePosition[0].y--;
-                break;
-            case "D":
-                snakePosition[0].y++;
-                break;
-        }
-        /* if snakePosition[0] enters apple then add new snake segment (last_x, last_y) */
-
-}
-
-function moveSnake() {
-    let snakeElement = document.getElementById('snake0');
-    if (snakeElement === null) {
-        let gameBoard = document.querySelector('.game-container');
-        for (let i = 0; i < snakePosition.length; i++) {
-            snakeElement = document.createElement('div');
-            snakeElement.style.gridRowStart = snakePosition[i].y;
-            snakeElement.style.gridColumnStart = snakePosition[i].x;
-            snakeElement.classList.add('snake-element');
-            snakeElement.id = "snake" + i.toString();
-            gameBoard.appendChild(snakeElement);
-        }
-    } else {
-        updatePosition();
-        for (let i = 0; i < snakePosition.length; i++) {
-            snakeElement = document.getElementById("snake" + i.toString())
-            snakeElement.style.gridRowStart = snakePosition[i].y;
-            snakeElement.style.gridColumnStart = snakePosition[i].x;
-        }
-
+    }
+  } else {
+    updatePosition();
+    for (let i = 0; i < snakePosition.length; i++) {
+      snakeElement = document.getElementById("snake" + i.toString());
+      snakeElement.style.gridRowStart = snakePosition[i].y;
+      snakeElement.style.gridColumnStart = snakePosition[i].x;
     }
   }
-
+}
 
 function serveDirection() {
-    window.addEventListener('keydown', event => {
-        switch (event.key) {
-            case 'ArrowUp':
-                if (direction === "D") break;
-                direction = "U";
-                break
-            case 'ArrowDown':
-                if (direction === "U") break;
-                direction = "D";
-                break
-            case 'ArrowLeft':
-                if (direction === "R") break;
-                direction = "L";
-                break
-            case 'ArrowRight':
-                if (direction === "L") break;
-                direction = "R";
-                break
-        }
-    })
+  window.addEventListener("keydown", (event) => {
+    switch (event.key) {
+      case "ArrowUp":
+        direction = "U";
+        break;
+      case "ArrowDown":
+        direction = "D";
+        break;
+      case "ArrowLeft":
+        direction = "L";
+        break;
+      case "ArrowRight":
+        direction = "R";
+        break;
+    }
+  });
 }
 
 function snakeSpeedAndMove() {
-    if (snakePosition.length >= 3) {
-        setInterval(moveSnake, 500);
-    } else if (snakePosition.length >= 10) {
-        setInterval(moveSnake, 250);
-    } else {
-        setInterval(moveSnake, 1000);
-    };
-};
+  if (snakePosition.length >= 3) {
+    setInterval(moveSnake, 500);
+  } else if (snakePosition.length >= 10) {
+    setInterval(moveSnake, 250);
+  } else {
+    setInterval(moveSnake, 1000);
+  }
+}
 
 let startButton = document.getElementById("start-game");
-function clickToStartGame () {
-    startButton.addEventListener('click', e=> {
-        e.currentTarget.style.visibility='hidden';
-        initGame();
-    });
-};
+function clickToStartGame() {
+  startButton.addEventListener("click", (e) => {
+    e.currentTarget.style.visibility = "hidden";
+    initGame();
+  });
+}
 
 clickToStartGame();
 
 function initGame() {
-    // Your game can start here, but define separate functions, don't write everything in here :)
-    serveDirection();
-    drawApple();
-    snakeSpeedAndMove();
+  // Your game can start here, but define separate functions, don't write everything in here :)
+  serveDirection();
+  drawApple();
+  snakeSpeedAndMove();
 }
 
 function createSnake() {
@@ -220,163 +175,163 @@ function createSnake() {
 }
 
 function customizeSnake() {
-    let snakeColor = "";
-    let snakeShape = "";
-    let borderColor = "";
-    const category = document.querySelector(".category-header");
+  let snakeColor = "";
+  let snakeShape = "";
+  let borderColor = "";
+  const category = document.querySelector(".category-header");
 
-    const deleteButtons = function () {
-        const buttons = Array.from(document.querySelectorAll("button"));
-        buttons.forEach((element) => {
-            element.remove();
+  const deleteButtons = function () {
+    const buttons = Array.from(document.querySelectorAll("button"));
+    buttons.forEach((element) => {
+      element.remove();
+    });
+  };
+
+  const buttons = (function () {
+    let x = 3;
+    let y = 2;
+    for (let i = 0; i < 9; ++i) {
+      const button = document.createElement("button");
+      button.classList.add("color-element");
+      button.style.gridColumn = `${x} / ${x + 4}`;
+      button.style.gridRow = `${y} / ${y + 4}`;
+      gameBoard.insertAdjacentElement("beforeend", button);
+
+      x += 6;
+
+      if (x == 21) {
+        x = 3;
+        y += 6;
+      }
+    }
+    return Array.from(document.querySelectorAll("button"));
+  })();
+
+  const startGame = function (chosenButton) {
+    snakeShape = chosenButton.style.borderRadius;
+    category.remove();
+    deleteButtons();
+    document.body.style.setProperty("--snakeColor", snakeColor);
+    document.body.style.setProperty("--snakeShape", snakeShape);
+    document.body.style.setProperty("--snakeBorderColor", borderColor);
+    initGame();
+  };
+
+  const chooseSnakeShape = function (chosenButton) {
+    category.textContent = "Choose snake shape";
+
+    borderColor = chosenButton.style.borderColor;
+
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].style.borderColor = borderColor;
+      buttons[i].animate([{ borderRadius: `${6.5 * i}%` }], 500);
+      setTimeout(() => {
+        buttons[i].style.borderRadius = `${6.5 * i}%`;
+      }, 450);
+
+      buttons[i].onclick = () => {
+        buttons.forEach((button) => {
+          button.onclick = null;
+          button.animate(
+            [{ borderRadius: buttons[i].style.borderRadius }],
+            500
+          );
         });
-    };
+        setTimeout(() => {
+          startGame(buttons[i]);
+        }, 450);
+      };
+    }
+  };
 
-    const buttons = (function () {
-        let x = 3;
-        let y = 2;
-        for (let i = 0; i < 9; ++i) {
-            const button = document.createElement("button");
-            button.classList.add("color-element");
-            button.style.gridColumn = `${x} / ${x + 4}`;
-            button.style.gridRow = `${y} / ${y + 4}`;
-            gameBoard.insertAdjacentElement("beforeend", button);
+  // const chooseBorderStyle = function (chosenButton) {
+  //   category.textContent = "Choose border style";
+  //
+  //   borderColor = chosenButton.style.borderColor;
+  //
+  //   const borderStyles = [
+  //     "hidden",
+  //     "dotted",
+  //     "dashed",
+  //     "solid",
+  //     "double",
+  //     "dotted solid",
+  //     "dotted dashed",
+  //     "double solid",
+  //     "solid double",
+  //   ];
+  //
+  //   for (let i = 0; i < buttons.length; i++) {
+  //     buttons[i].style.borderColor = borderColor;
+  //     buttons[i].style.borderStyle = borderStyles[i];
+  //     buttons[i].onclick = chooseSnakeShape;
+  //   }
+  // };
 
-            x += 6;
+  const chooseBorderColor = function (chosenButton) {
+    category.textContent = "Choose border color";
 
-            if (x == 21) {
-                x = 3;
-                y += 6;
-            }
-        }
-        return Array.from(document.querySelectorAll("button"));
-    })();
+    snakeColor = chosenButton.style.backgroundColor;
 
-    const startGame = function (chosenButton) {
-        snakeShape = chosenButton.style.borderRadius;
-        category.remove();
-        deleteButtons();
-        document.body.style.setProperty("--snakeColor", snakeColor);
-        document.body.style.setProperty("--snakeShape", snakeShape);
-        document.body.style.setProperty("--snakeBorderColor", borderColor);
-        initGame();
-    };
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].style.backgroundColor = snakeColor;
+      buttons[i].style.borderStyle = "solid";
+      buttons[i].style.borderColor = "transparent";
 
-    const chooseSnakeShape = function (chosenButton) {
-        category.textContent = "Choose snake shape";
+      const borderColor =
+        i === 1 ? "transparent" : `rgb(${i * 28}, ${i * 28}, ${i * 28})`;
 
-        borderColor = chosenButton.style.borderColor;
+      buttons[i].animate(
+        [
+          {
+            borderColor: borderColor,
+          },
+        ],
+        500
+      );
+      setTimeout(() => {
+        buttons[i].style.borderColor = borderColor;
+      }, 450);
 
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].style.borderColor = borderColor;
-            buttons[i].animate([{borderRadius: `${6.5 * i}%`}], 500);
-            setTimeout(() => {
-                buttons[i].style.borderRadius = `${6.5 * i}%`;
-            }, 450);
+      buttons[i].onclick = () => {
+        buttons.forEach((button) => {
+          button.onclick = null;
+          button.animate([{ borderColor: buttons[i].style.borderColor }], 500);
+        });
+        setTimeout(() => {
+          chooseSnakeShape(buttons[i]);
+        }, 450);
+      };
+    }
+  };
 
-            buttons[i].onclick = () => {
-                buttons.forEach((button) => {
-                    button.onclick = null;
-                    button.animate(
-                        [{borderRadius: buttons[i].style.borderRadius}],
-                        500
-                    );
-                });
-                setTimeout(() => {
-                    startGame(buttons[i]);
-                }, 450);
-            };
-        }
-    };
-
-    // const chooseBorderStyle = function (chosenButton) {
-    //   category.textContent = "Choose border style";
-    //
-    //   borderColor = chosenButton.style.borderColor;
-    //
-    //   const borderStyles = [
-    //     "hidden",
-    //     "dotted",
-    //     "dashed",
-    //     "solid",
-    //     "double",
-    //     "dotted solid",
-    //     "dotted dashed",
-    //     "double solid",
-    //     "solid double",
-    //   ];
-    //
-    //   for (let i = 0; i < buttons.length; i++) {
-    //     buttons[i].style.borderColor = borderColor;
-    //     buttons[i].style.borderStyle = borderStyles[i];
-    //     buttons[i].onclick = chooseSnakeShape;
-    //   }
-    // };
-
-    const chooseBorderColor = function (chosenButton) {
-        category.textContent = "Choose border color";
-
-        snakeColor = chosenButton.style.backgroundColor;
-
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].style.backgroundColor = snakeColor;
-            buttons[i].style.borderStyle = "solid";
-            buttons[i].style.borderColor = "transparent";
-
-            const borderColor =
-                i === 1 ? "transparent" : `rgb(${i * 28}, ${i * 28}, ${i * 28})`;
-
-            buttons[i].animate(
-                [
-                    {
-                        borderColor: borderColor,
-                    },
-                ],
-                500
-            );
-            setTimeout(() => {
-                buttons[i].style.borderColor = borderColor;
-            }, 450);
-
-            buttons[i].onclick = () => {
-                buttons.forEach((button) => {
-                    button.onclick = null;
-                    button.animate([{borderColor: buttons[i].style.borderColor}], 500);
-                });
-                setTimeout(() => {
-                    chooseSnakeShape(buttons[i]);
-                }, 450);
-            };
-        }
-    };
-
-    (function chooseSnakeColor() {
-        const colors = [
-            "#ef476f",
-            "#ffd166",
-            "#06d6a0",
-            "#118ab2",
-            "#073b4c",
-            "#c19ab7",
-            "#9c95dc",
-            "#dcc48e",
-            "#605b56",
-        ];
-        category.textContent = "Choose snake color";
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].style.backgroundColor = colors[i];
-            buttons[i].onclick = () => {
-                buttons.forEach((button) => {
-                    button.onclick = null;
-                    button.animate(
-                        [{backgroundColor: buttons[i].style.backgroundColor}],
-                        500
-                    );
-                });
-                setTimeout(() => {
-                    chooseBorderColor(buttons[i]);
-                }, 450);
-            };
-        }
-    })();
-};
+  (function chooseSnakeColor() {
+    const colors = [
+      "#ef476f",
+      "#ffd166",
+      "#06d6a0",
+      "#118ab2",
+      "#073b4c",
+      "#c19ab7",
+      "#9c95dc",
+      "#dcc48e",
+      "#605b56",
+    ];
+    category.textContent = "Choose snake color";
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].style.backgroundColor = colors[i];
+      buttons[i].onclick = () => {
+        buttons.forEach((button) => {
+          button.onclick = null;
+          button.animate(
+            [{ backgroundColor: buttons[i].style.backgroundColor }],
+            500
+          );
+        });
+        setTimeout(() => {
+          chooseBorderColor(buttons[i]);
+        }, 450);
+      };
+    }
+  })();
+}
